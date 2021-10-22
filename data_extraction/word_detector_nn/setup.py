841b52e0638556dd72088c3adcef9f2890600cac
jachemharlyn <hmanlapaz@agsx.net>
2021-10-22 22:15:43 +0800

    change setup.py

--- data_extraction/word_detector_nn/setup.py
+++ data_extraction/word_detector_nn/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='word_detect',
       version='0.1',
       description='The funniest joke in the world',
-      url='https://github.com/jachemharlyn/harlenest.git',
+      url='https://github.com/jachemharlyn/harlenest/tree/main/data_extraction/word_detector_nn',
       author='Flying Circus',
       author_email='flyingcircus@example.com',
       license='MIT License',
