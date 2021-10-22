b402469e26a2956b57af5323d6eaec6beeec1122
jachemharlyn <hmanlapaz@agsx.net>
2021-10-22 20:53:13 +0800

    test

---
+++ word_nn/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='harlenest',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/jachemharlyn/harlenest.git',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT License',
+      packages=['word_nn'],
+      zip_safe=False)
